aecfa773a038f4fc4c65e77144c6d6b2ca29b182
rajibalmahmud <138250137+rajibalmahmud@users.noreply.github.com>
2025-08-24 08:14:32 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+});
